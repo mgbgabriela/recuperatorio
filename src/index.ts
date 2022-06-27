e5828ecd3c4c8eec4aa618b19ed5f5737342eaab
Gabriela <104107313+mgbgabriela@users.noreply.github.com>
2022-06-27 14:25:24 -0300

    ingreso-función-mostrar-beneficio

--- src/index.ts
+++ src/index.ts
@@ -18,32 +18,71 @@ Finalizada la compra, el programa va a mostrar por pantalla el
 • si compra a partir de 1000 pero menos de 2000 participa un tv led
 • si compra a partir de 2000 pero menos de 3000 participa por una moto 0k
 • si compra a partir de 3000 participa por un 0km*/
+
+//Declaro variables
+
 let dimension: number = Number(
-  prompt("Indique la cantidad de productos que desea ingresar"));
-let productos : string[] = new Array (dimension);
-let precios : number[] = new Array (dimension);
-let cantidadesProductos : number[] = new Array (dimension); 
-let subtotales : number[] = new Array (dimension);
+  prompt("Indique la cantidad de productos que desea ingresar")
+);
+let productos: string[] = new Array(dimension);
+let precios: number[] = new Array(dimension);
+let cantidadesProductos: number[] = new Array(dimension);
+let subtotales: number[] = new Array(dimension);
 
+//Realizo función para cargar productos y sumar total
+let cargarCompra = (
+  dimension: number,
+  producto: string[],
+  precio: number[],
+  cantidadProducto: number[],
+  subtotal: number[]
+) => {
+  let indice: number;
+  let total: number = 0;
 
-let cargarCompra = (dimension: number, producto: string[],
-   precio: number[], cantidadProducto: number[], subtotal : number[]) => {
-    let indice : number;
-    let total : number = 0;
-
-    for (indice = 0; indice < dimension; indice ++){
-    producto[indice] = prompt ("Ingrese el nombre del producto");
-    precio[indice]= Number(prompt("Ingrese el valor para el vector" + indice));
-    cantidadProducto[indice]= Number (prompt ("Indique la cantidad de productos"));
+  for (indice = 0; indice < dimension; indice++) {
+    producto[indice] = prompt("Ingrese el nombre del producto");
+    precio[indice] = Number(prompt("Ingrese el valor para el vector" + indice));
+    cantidadProducto[indice] = Number(
+      prompt("Indique la cantidad de productos")
+    );
     subtotal[indice] = precio[indice] * cantidadProducto[indice];
-    
+
     total += subtotal[indice];
-    }
-    console.log (producto + " precio unitario " + precio + " x " 
-    + cantidadProducto + " subtotal " + subtotal + " el total es de $ " + total);
-    return total;
-   
-}
+  }
+  console.log(
+    producto +
+      " precio unitario " +
+      precio +
+      " x " +
+      cantidadProducto +
+      " subtotal " +
+      subtotal
+  );
+  return total;
+};
 
+let total: number = cargarCompra(
+  dimension,
+  productos,
+  precios,
+  cantidadesProductos,
+  subtotales
+);
+console.log("El total es " + total);
 
-cargarCompra (dimension, productos, precios, cantidadesProductos, subtotales));
+let mostrarBeneficio = (total: number) => {
+  if (total >= 3000) {
+    console.log("Usted participa por un 0 Km");
+  }
+  if (total >= 2000 && total < 3000) {
+    console.log("Usted participa por una moto 0 Km");
+  }
+  if (total >= 1000 && total < 2000) {
+    console.log("Usted participa por un tv Led");
+  } else {
+    console.log("Gracias por su compra");
+  }
+};
+
+mostrarBeneficio(total);
